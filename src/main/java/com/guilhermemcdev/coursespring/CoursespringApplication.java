1540a43a15acfd1ef4b9a3e7d01ddeaf9f320204
Guilherme-mendesc <guimendes1337@gmail.com>
2023-03-10 16:08:41 -0300

    service layer, component registration

--- src/main/java/com/guilhermemcdev/coursespring/CoursespringApplication.java
+++ src/main/java/com/guilhermemcdev/coursespring/CoursespringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoursespringApplication {
-//parei na aula de 299
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoursespringApplication.class, args);
 	}
